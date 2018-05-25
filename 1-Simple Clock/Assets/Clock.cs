96ac1144193df894f3b61b8192ae4b6d692a9703
theysp <theysp@qq.com>
2018-05-26 00:08:50 +0800

    clock finished

--- 1-Simple Clock/Assets/Clock.cs
+++ 1-Simple Clock/Assets/Clock.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Clock : MonoBehaviour {
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    float degreePerHour = 30f;
+    float degreePerMinute = 6f;
+    float degreePerSecond = 6f;
+    private void Awake()
+    {
+        hoursTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreePerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreePerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second * degreePerSecond, 0f);
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        DateTime now = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, now.Hour * degreePerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, now.Minute * degreePerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, now.Second * degreePerSecond, 0f);
+    }
 }
