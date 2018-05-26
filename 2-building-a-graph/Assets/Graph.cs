c0b7d2ef9948fad361d5bef3e807b4d9f4999fd9
theysp <theysp@qq.com>
2018-05-26 08:17:01 +0800

    2-building-graph

---
+++ 2-building-a-graph/Assets/Graph.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Graph : MonoBehaviour {
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        for(int i=0;i<10;++i)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right*((i+0.5f)/5f-1f);
+            point.localScale = Vector3.one / 5f;
+            i += 1;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
