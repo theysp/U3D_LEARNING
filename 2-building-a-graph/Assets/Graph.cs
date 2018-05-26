5aa3416f2db8c90ab8f7bc04ff04e3f83f5ef9ec
theysp <theysp@qq.com>
2018-05-26 12:33:35 +0800

    2-finished

--- 2-building-a-graph/Assets/Graph.cs
+++ 2-building-a-graph/Assets/Graph.cs
@@ -3,23 +3,49 @@ using System.Collections;
 
 public class Graph : MonoBehaviour {
     public Transform pointPrefab;
-
+    public int resolution = 10;
+    public float xmin = -1f;
+    public float xmax = 1f;
+    Transform[] points;
+    int updateCount=0;
     private void Awake()
     {
-        for(int i=0;i<10;++i)
+        points = new Transform[resolution];
+        if(resolution < 2)
+            Debug.Log("resolution not valid"+resolution);
+        Vector3 pos;
+        pos.z = 0f;
+        float step = (xmax - xmin) / resolution;
+        for (int i = 0; i < resolution; ++i)
         {
-            Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right*((i+0.5f)/5f-1f);
-            point.localScale = Vector3.one / 5f;
+            float curx = xmin+step*i;
+            points[i] = Instantiate(pointPrefab);
+            pos.x = curx;
+            pos.y = curx*curx;
+            points[i].localPosition = pos;
+            points[i].localScale = Vector3.one / resolution;
+            points[i].SetParent(this.transform, false);
         }
     }
     // Use this for initialization
     void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        Vector3 pos;
+        updateCount++;
+        pos.z = 0f;
+        float step = (xmax - xmin) / resolution;
+        for (int i = 0; i < resolution; ++i)
+        {
+            float curx = xmin + step * i;
+            pos.x = curx;
+            pos.y = Mathf.Sin((curx + updateCount*0.01f)*5);
+            points[i].localPosition = pos;
+            points[i].localScale = Vector3.one / resolution;
+            
+        }
 	}
 }
